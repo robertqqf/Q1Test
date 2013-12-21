5a367ed25b0d930bfdc78b913f97cd53cc7921eb
elico <elico_yqf@sina.com>
2013-12-21 21:56:18 +0800

    添加了省份标识，可以对多个省份进行识别，目前添加了青海的西宁分公司帐号。 添加了省份标识，可以对多个省份进行识别，目前添加了青海的西宁分公司帐号。 修正编号为空的bug.

--- app/models/test_db.rb
+++ app/models/test_db.rb
@@ -1,4 +1,4 @@
 class TestDb < ActiveRecord::Base
-  validates :city_code, :uniqueness => { :scope => [:cs,:year,:month] }
+  validates :city_code, :uniqueness => { :scope => [:cs,:year,:month,:province] }
   belongs_to :user
 end
